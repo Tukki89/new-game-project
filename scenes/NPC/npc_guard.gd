4a91bd15025d5aaac7e47e9dfef832e829b52154
Tukki89 <sonigear89@gmail.com>
2025-03-22 19:46:10 +0800

    dialogue system

--- scenes/NPC/npc_guard.gd
+++ scenes/NPC/npc_guard.gd
@@ -12,6 +12,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("chat"):
 		if is_chatting:
 			print("Already chatting with NPC")
+			$Dialogue.start()
 		else:
 			print("Chat triggered!")  
 			is_chatting = true  
@@ -25,3 +26,9 @@ func _on_chat_detection_area_body_entered(body: Node2D) -> void:
 func _on_chat_detection_area_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		print("Player exited chat zone")
+
+# Triggered when the dialogue finishes
+func _on_dialogue_dialogue_finished():
+	is_chatting = false  # Reset chat state
+	animated_sprite.play("Idle")  # Return to idle animation
+	print("Dialogue finished. Chatting:", is_chatting)
