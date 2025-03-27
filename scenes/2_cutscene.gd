578cab9e51eda88c976b6c8060783d4c40d6f104
Tukki89 <sonigear89@gmail.com>
2025-03-28 07:19:51 +0800

    Revert "Merge branch 'main' of https://github.com/Tukki89/new-game-project"
    This reverts commit 723489eb6217a959ef6960840e3ed536d3c59c27, reversing
    changes made to 4c0bcdadcdd1a1f3b2da45e345115dd716ead272.

--- scenes/2_cutscene.gd
+++
@@ -1,9 +0,0 @@
-extends Control  # Now it's attached to the Control node
-
-@export var next_scene: String = "res://scenes/new world.tscn"
-
-func _ready():
-	var video_player = $VideoStreamPlayer  # Get the child VideoStreamPlayer node
-	video_player.play()  # Start the video
-	await video_player.finished  # Wait for the video to finish
-	get_tree().change_scene_to_file(next_scene)  # Change to the next scene
